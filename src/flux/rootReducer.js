6fd79481b52e9bbd1f5afd4fc335b37a0c14139b
Seth Martin <sethmartin@Seths-MacBook-Pro.local>
2019-09-26 13:08:45 -0700

    implements selector and directory state

--- src/flux/rootReducer.js
+++ src/flux/rootReducer.js
@@ -1,8 +1,20 @@
 import cartReducer from 'flux/reducers/cartReducer';
 import { combineReducers } from 'redux';
+import directoryReducer from 'flux/reducers/directoryReducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from 'flux/reducers/userReducer';
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './index.css';
 
+import { persistor, store } from 'flux/store';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from 'flux/store';
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
